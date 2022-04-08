d889bd3d3ec88ecec093380f53396cfce20c0bef
Carl Jones <carljones9411@gmail.com>
2022-04-08 11:16:51 +0100

    added product context and started making the shop page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import { Route, Routes } from "react-router-dom";
 
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+import Shop from "./routes/shop/shop.component";
 
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return <h1>I am the shop Page</h1>;
-};
 
 const App = () => {
   return (
